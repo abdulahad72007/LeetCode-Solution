43324cbfb4032036e0574666b328fc6f693bc22c
abdulahad72007 <abdulahad72007@gmail.com>
2026-04-04 00:27:21 +0500

    Day 4: I solved the Longest Common Prefix Quiz.

---
+++ 4_Longest_Common_Prefix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() -1);
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<string> words = {"Flower", "Flow", "Flight"};
+    string result = obj.longestCommonPrefix(words);
+
+    cout<<"Longest Common Prefix is: " << result << endl;
+    return 0;
+}
